7489298a2ce14472e29aee8289689b336b2a1d36
andresanunezt <andresanunez98@gmail.com>
2022-07-22 15:21:54 -0400

    commit

--- seed.js
+++ seed.js
@@ -1,38 +1,39 @@
-const path = require('path');
-const fs = require('fs').promises;
-const bcrypt = require('bcrypt');
+const path = require("path");
+const fs = require("fs").promises;
+const bcrypt = require("bcrypt");
 
-const {sequelize} = require('./db');
-const {User, Item} = require('./models');
+const { sequelize } = require("./db");
+const { User, Item } = require("./models");
 
 const createUsers = async () => {
+  const users = [
+    { name: "Dan", password: "1234" },
+    { name: "Linda", password: "password" },
+    { name: "Andés", password: "password" },
+    { name: "Brad", password: "password" },
+    { name: "Rene", password: "password" },
+    { name: "RZ", password: "password" },
+    { name: "Stanley", password: "password" },
+    { name: "Constance", password: "password" },
+    { name: "Jin", password: "password" },
+    { name: "Shafee", password: "password" },
+    { name: "Diana", password: "password" },
+  ];
 
-    const users = [
-        {name : 'Dan', password: '1234' },
-        {name : 'Linda', password : 'password'}
-    ];
+  return users;
+};
 
-    return users
-}
-
-
-const items = [
-    {name : 'Gold'},
-    {name : 'Silver'},
-    {name : 'Paladium'}
-];
-
+const items = [{ name: "Gold" }, { name: "Silver" }, { name: "Paladium" }];
 
 const seed = async () => {
+  await sequelize.sync({ force: true });
 
-    await sequelize.sync({ force: true });
+  const users = await createUsers(); // create users w/ encrypted passwords
 
-    const users = await createUsers(); // create users w/ encrypted passwords
-
-    const userPromises = users.map(user => User.create(user))
-    const itemPromises = items.map(item => Item.create(item))
-    await Promise.all([...userPromises, ...itemPromises]);
-    console.log("db populated!")
-}
+  const userPromises = users.map((user) => User.create(user));
+  const itemPromises = items.map((item) => Item.create(item));
+  await Promise.all([...userPromises, ...itemPromises]);
+  console.log("db populated!");
+};
 
 seed();
